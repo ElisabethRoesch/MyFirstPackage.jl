d1ffb1f80f36f12cbf5ded42d548886cdf735059
LislPisl <eroesch@student.unimelb.edu.au>
2020-06-12 09:53:11 +1000

    add test comment

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -2,7 +2,7 @@ module MyFirstPackage
     # Here I list all the packages that I will use in this package.
     using DifferentialEquations, Plots
     # Here I list all the files I want to include.
-    include("myFile1.jl")
+    include("myFile1.jl") # first file
     include("myFile2.jl")
     include("myFile3.jl")
     # Here I list all the methods, I want to provide to the user.
